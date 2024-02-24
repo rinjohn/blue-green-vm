0a022573de1a6743155451799022e2aa26e223ff
rinjohn <43170752+rinjohn@users.noreply.github.com>
2024-02-24 17:06:22 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,7 +17,7 @@ terraform {
 #  # Update this block with the location of your terraform state file
     backend "azurerm" {
       resource_group_name  = "rg-terraform-github-actions-state"
-      storage_account_name = "terraformgithubactions"
+      storage_account_name = "terraformgithubactions004748"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
       use_oidc             = true
